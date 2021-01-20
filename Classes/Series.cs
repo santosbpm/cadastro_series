dcbfe42449db11f39bade41cfb16c4661031a469
santosbpm <santosbpm.0@gmail.com>
2021-01-19 21:50:05 -0300

    Crinado classe Serie

---
+++ Classes/Series.cs
@@ -0,0 +1,21 @@
+namespace cadastro_series
+{
+    public class Series : EntidadeBase 
+    {
+        // Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        // Métodos
+        public Series(int id, Genero genero, string titulo, string descricao, tin ano)
+        {
+            this.ID = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+    }
+}
