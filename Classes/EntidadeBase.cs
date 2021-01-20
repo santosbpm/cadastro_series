4b9be9b5c85700147cc38fddc620c6e77e1adad9
santosbpm <santosbpm.0@gmail.com>
2021-01-19 21:48:34 -0300

    Adicionando base do projeto e criando classe Classe/EntidadeBase

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace cadastro_series
+{
+    public abstract class EntidadeBase
+    {
+        public int ID {get; protected set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cadastro_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
