4365bb28b3e449ea95e5a9f90dd1954762359c4d
santosbpm <santosbpm.0@gmail.com>
2021-01-19 22:01:24 -0300

    Modificando classe Series, sobrescrevendo metodo ToString para ter uma saida personalizada

--- Classes/Series.cs
+++ Classes/Series.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace cadastro_series
 {
     public class Series : EntidadeBase 
@@ -16,6 +18,16 @@ namespace cadastro_series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            return retorno;
         }
     }
 }
