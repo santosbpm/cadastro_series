fe6cc15704d1ee471dd5774e6575390b5aa53aa6
santosbpm <santosbpm.0@gmail.com>
2021-01-19 22:11:01 -0300

    Interface de repositorio do projeto

---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace cadastro_series.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+         List<T> Lista();
+         T RetornaPorID(int id);
+         void Insere(T entidade);
+         void Exclui(int id);
+         void Atualiza(int id, T entidade);
+         int ProximoID();
+    }
+}
